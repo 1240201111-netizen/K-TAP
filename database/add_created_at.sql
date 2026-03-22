e4fc483f28ce0f60121d81290b40de0dc465f5e2
1240201111-netizen <1240201111@nku.edu.tr>
2026-03-22 23:32:43 +0300

    Delete database/add_created_at.sql

--- database/add_created_at.sql
+++
@@ -1,4 +0,0 @@
-alter table public.books add column if not exists created_at timestamp with time zone default now();
-alter table public.suggestions add column if not exists created_at timestamp with time zone default now();
-alter table public.polls add column if not exists created_at timestamp with time zone default now();
-alter table public.interviews add column if not exists created_at timestamp with time zone default now();
